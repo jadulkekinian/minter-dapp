9462c03cf62e6c96ad9c8791975f32500dba0a6a
Jadulkekinian <jadulkekinian@gmail.com>
2022-04-08 21:04:02 +0700

    wl

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0x4fB4BAd770454e8A46A5a592EE8a53bCe8998658"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x4fB4BAd770454e8A46A5a592EE8a53bCe8998658"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
